e2e030ecadec12a166c51ba050eb4e1ee4f51dfb
Henning Jacobs <henning@jacobs1.de>
2014-09-15 14:04:46 +0200

    no requirements.txt needed

--- setup.py
+++ setup.py
@@ -6,27 +6,21 @@ import setuptools
 
 __location__ = os.path.join(os.getcwd(), os.path.dirname(inspect.getfile(inspect.currentframe())))
 
-def get_install_requirements(path):
-    content = open(os.path.join(__location__, path)).read()
-    return [req for req in content.split('\\n') if req != '']
-
 def read(fname):
     return open(os.path.join(__location__, fname)).read()
 
 def setup_package():
-    install_reqs = get_install_requirements('requirements.txt')
     setuptools.setup(
         name='nsenter',
-        version='0.1',
+        version='0.1.1',
         url='https://github.com/zalando/python-nsenter',
-        description='',
+        description='Enter kernel namespaces from Python',
         author='Henning Jacobs',
         author_email='henning.jacobs@zalando.de',
         long_description=read('README.rst'),
         classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python'],
         test_suite='tests',
         packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
-        install_requires=install_reqs,
         entry_points={'console_scripts': ['nsenter = nsenter:main']}
     )
 
